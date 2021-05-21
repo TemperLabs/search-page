0a1f9b9d0bb11cdcd6f6a1278e381c11a15da647
Maksim <temperlabs@gmail.com>
2021-05-21 17:36:56 +0300

    first commit

---
+++ src/components/SearchPage.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="home">
+    <vk-navbar>
+      <vk-navbar-nav>
+          <vk-navbar-item>
+          <vk-navbar-nav-item icon="search"></vk-navbar-nav-item>
+          <form class="uk-search uk-search-navbar" action="javascript:void(0)">
+            <input class="uk-input uk-form-width-small uk-margin-small-right" type="text" placeholder="Input" v-model="inputText">
+            <vk-button type="primary" @click="handleSearch(inputText)">Find</vk-button>
+          </form>
+        </vk-navbar-item>
+      </vk-navbar-nav>
+    </vk-navbar>
+    <searchResult v-if="searchDone"/>
+  </div>
+</template>
+
+<script>
+import searchResult from './SearchResult'
+export default {
+  name: 'Home',
+  components: {
+    searchResult
+  },
+  data () {
+    return {
+      inputText: '',
+      searchDone: false
+    }
+  },
+  methods: {
+    handleSearch (text) {
+      const search = {
+        query: '',
+        text: text
+      }
+      if (this.validEmail(text)) {
+        search.query = 'email'
+      }
+      if (this.validIP(text)) {
+        search.query = 'IP'
+      }
+      if (this.validName(text)) {
+        search.query = 'name'
+      }
+      if (this.validFullname(text)) {
+        search.query = 'fullname'
+      }
+      if (this.validPhone(text)) {
+        search.query = 'phone'
+        search.text = text.replace(/[^a-zA-Z0-9 ]/g, '')
+      }
+      this.$store.dispatch('searchData', search)
+      this.searchDone = true
+    },
+    validEmail: function (email) {
+      const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      return re.test(email)
+    },
+    validIP: function (IP) {
+      const re = /^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/
+      return re.test(IP)
+    },
+    validName: function (name) {
+      const re = /^(@)?[A-Za-z0-9-_]{3,24}$/
+      return re.test(name)
+    },
+    validFullname: function (Fullname) {
+      const re = /^([a-zA-Z]{2,}\s[a-zA-Z]+'?-?[a-zA-Z]{2,}\s?([a-zA-Z]{1,})?)$/
+      return re.test(Fullname)
+    },
+    validPhone: function (phone) {
+      const re = /^(\+)?([-\s()]?\d[-()\s]?){10,14}(\s*)?$/
+      return re.test(phone)
+    }
+  }
+}
+</script>
---
+++ src/components/SearchResult.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <p>
+      <vk-button type="primary" size="large">Large button</vk-button>
+    </p>
+    <vk-grid class="uk-child-width-1-2 uk-text-center">
+      <div>
+        <vk-card>Item</vk-card>
+      </div>
+      <div>
+        <vk-grid class="uk-child-width-1-2 uk-text-center">
+          <div>
+            <vk-card padding="large">
+              <vk-card-title>Large</vk-card-title>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </vk-card>
+          </div>
+          <div>
+            <vk-card padding="large">
+              <vk-card-title>Large</vk-card-title>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </vk-card>
+          </div>
+        </vk-grid>
+      </div>
+    </vk-grid>
+  </div>
+</template>
